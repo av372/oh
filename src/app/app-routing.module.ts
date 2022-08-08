0ac7e028d7d76da46513bd89515d37d97eaa1685
av372 <av372@njit.edu>
2022-08-08 20:26:40 +0530

    Contact clean up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BreedlistComponent } from './components/breedlist/breedlist.component';
-import { ContactComponent } from './components/contact/contact.component'
+import { ContactComponent } from './components/contact/contact.component';
 
 const routes: Routes = [
-  {path: 'breeds', component: BreedlistComponent},
-  {path: 'contact', component: ContactComponent },
-  {path:'', redirectTo:'breeds', pathMatch:'full'}
+  { path: 'breeds', component: BreedlistComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: 'breeds', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
